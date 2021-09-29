f6d20356d1eaca1b3d936de6d482701bef9ccd10
akhil v j <you@example.com>
2021-09-29 11:54:48 +0530

    changes added

---
+++ js/script.js
@@ -0,0 +1,22 @@
+(function($) {
+  function slickcarosal() {
+    $('.slider-for').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: true,
+      fade: true,
+      asNavFor: '.slider-nav'
+    });
+    $('.slider-nav').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      asNavFor: '.slider-for',
+      dots: true,
+      centerMode: true,
+      focusOnSelect: true
+    });
+  }
+  $(document).ready(function() {
+    slickcarosal();
+  });
+})(jQuery);
